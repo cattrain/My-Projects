5156feb881ab66d8f73dc0ab365d818a768f5e88
cattrain <cattrain@gmail.com>
2014-04-28 11:58:48 -0500

    version changes
    I'm still working on interpreting my version information into human
    readable information. I'm beginning to learn about pointers, but I still
    don't really understand how to read the information from
    AutoVersion::MONTH

--- learn.h
+++ learn.h
@@ -9,47 +9,25 @@ using namespace std;
 
 string VersionStyling(){
 
-    int input = AutoVersion::MONTH;
+    int input;
+    input = *AutoVersion::MONTH;
     string Month;
 
-    if (input == 1){
-        Month = "January";
-    }
-    else if (input == 2){
-        Month = "February";
-    }
-    else if (input == 3){
-        Month = "March";
-    }
-    else if (input == 4){
-        Month = "April";
-    }
-    else if (input == 5){
-        Month = "May";
-    }
-    else if (input == 6){
-        Month = "June";
-    }
-    else if (input == 7){
-        Month = "July";
-    }
-    else if (input == 8){
-        Month = "August";
-    }
-    else if (input == 9){
-        Month = "September";
-    }
-    else if (input == 10){
-        Month = "October";
-    }
-    else if (input == 11){
-        Month = "November";
-    }
-    else if (input == 12){
-        Month = "December";
-    }
-    else{
-        Month = "ERROR";
+    cout << *AutoVersion::MONTH << " " << &AutoVersion::MONTH << " " << AutoVersion::MONTH << " " << input << " test" << endl;
+
+    switch(input){
+        case 01: Month ="January";
+        case 02: Month ="February";
+        case 03: Month ="March";
+        case 04: Month ="April";
+        case 05: Month ="May";
+        case 06: Month ="June";
+        case 07: Month ="July";
+        case 8: Month ="August";
+        case 9: Month ="September";
+        case 10: Month ="October";
+        case 11: Month ="November";
+        case 12: Month ="December";
     }
 
     return Month;
--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "26";
+	static const char DATE[] = "28";
 	static const char MONTH[] = "04";
 	static const char YEAR[] = "2014";
 	static const char UBUNTU_VERSION_STYLE[] =  "14.04";
@@ -15,18 +15,18 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR  = 0;
-	static const long MINOR  = 0;
-	static const long BUILD  = 72;
-	static const long REVISION  = 69;
+	static const long MINOR  = 1;
+	static const long BUILD  = 14;
+	static const long REVISION  = 94;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 88;
-	#define RC_FILEVERSION 0,0,72,69
-	#define RC_FILEVERSION_STRING "0, 0, 72, 69\0"
-	static const char FULLVERSION_STRING [] = "0.0.72.69";
+	static const long BUILDS_COUNT  = 150;
+	#define RC_FILEVERSION 0,1,14,94
+	#define RC_FILEVERSION_STRING "0, 1, 14, 94\0"
+	static const char FULLVERSION_STRING [] = "0.1.14.94";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 72;
+	static const long BUILD_HISTORY  = 15;
 	
 
 }
